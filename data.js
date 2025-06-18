d465b462574780913ddbdbbe63318048720d1b64
shimsdebims <shimasarah777@gmail.com>
2025-06-18 13:36:28 +0000

    Fix image references: update image10.jpg to image10.jpeg and add new images image20.jpeg and image21.jpeg to gallery

--- data.js
+++ data.js
@@ -45,7 +45,8 @@ const songList = [
 
 const imageFiles = [
   "image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg", 
-  "image6.jpg", "image7.jpg", "image8.jpg", "image9.jpg", "image10.jpg", 
+  "image6.jpg", "image7.jpg", "image8.jpg", "image9.jpg", "image10.jpeg", 
   "image11.jpg", "image12.jpg", "image13.jpg", "image14.jpg", "image15.jpg", 
-  "image16.jpg", "image17.jpg", "image18.jpg", "image19.jpg"
+  "image16.jpg", "image17.jpg", "image18.jpg", "image19.jpg", "image20.jpeg", 
+  "image21.jpeg"
 ];
